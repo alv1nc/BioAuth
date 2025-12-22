d19530d183a56c2798d1d6f28cd1a1d3002bc39b
Alvin <alvinsanu.p@gmail.com>
2025-12-23 02:15:28 +0530

    api functions

---
+++ core/config.py
@@ -0,0 +1,18 @@
+import os 
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    API_NAME: str = "Bio-Auth API"
+    DEBUG_MODE: bool = True
+
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+    DB_USER: str
+    DB_PASS: str
+
+    SPEECHBRAIN_DIR: str = os.path.join(os.getcwd(),"model_weights")
+
+    class Config:
+        env_file = ".env"
+settings = Settings()
